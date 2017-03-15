38283a4984fccbac4582f1590bdce350673b9ef5
shi <binjieshi.cs@qq.com>
2017-03-15 20:22:18 +0800

    surprise库学习

--- test.py
+++ test.py
@@ -1,2 +1,17 @@
 # -*- coding: UTF-8 -*-
 
+from surprise import Dataset
+from surprise import Reader
+from surprise import SVD
+from surprise import evaluate, print_perf
+
+reader = Reader(line_format='user item rating', sep='|')
+
+data = Dataset.load_from_file(r'H:\ratings.dat', reader)
+data.split(2)
+
+algo = SVD()
+
+perf = evaluate(algo, data)
+
+print_perf(perf)
